1a2a02fd769051d4505572c8a823284f0de9f34f
ro_hit$choudhary <65584555+Rohit20016@users.noreply.github.com>
2026-03-31 11:53:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import request
+from flask import Flask, request
+import sqlite3
+
+app = Flask(__name__)
+
+def get_user(username):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM users WHERE name = '{username}'")
+    return cursor.fetchall()
 
 @app.route("/user")
 def user():
     username = request.args.get("username")
-    return get_user(username)
+    return str(get_user(username))
